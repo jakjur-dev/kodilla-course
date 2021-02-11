33a73f8477e66f598323441fbe7747d24bc5d3dc
jakjur-dev <jakub.jurczak46@gmail.com>
2021-02-11 12:26:55 +0100

    6.2 Added calculator and calculator tests

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    public static int subtract(int a, int b) {
+        int c = a - b;
+        return c;
+    }
+}
