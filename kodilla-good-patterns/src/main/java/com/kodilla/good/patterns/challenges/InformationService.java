9ea79ff8455779292a02e45c1cfaec39682cadaf
jakjur-dev <jakub.jurczak46@gmail.com>
2021-04-01 21:16:25 +0200

    13.2 Added OrderServiceApplication

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationService.java
@@ -3,6 +3,5 @@ package com.kodilla.good.patterns.challenges;
 public interface InformationService {
 
     default void inform(User user){
-        System.out.println("Messege sent through SMS");
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderService.java
@@ -1,7 +1,5 @@
 package com.kodilla.good.patterns.challenges;
 
-import java.time.LocalDateTime;
-
 public interface OrderService {
 
     default boolean order(User user, Product product, String address){
