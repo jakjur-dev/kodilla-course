e1bc4fbdbf42c660a92d8e8dcb72b60e9fbdfbae
jakjur-dev <jakub.jurczak46@gmail.com>
2021-04-10 18:17:11 +0200

    14.3 Added Board and BoardTestSuite

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BoardTestSuite {
+
+    @Test
+    void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+
+        //When
+        board.getToDoList().addTask("task to do");
+        board.getInProgressList().addTask("task in progress");
+        board.getDoneList().addTask("done task");
+
+        //Then
+        System.out.println(board.getToDoList().getTask(0));
+        System.out.println(board.getInProgressList().getTask(0));
+        System.out.println(board.getDoneList().getTask(0));
+    }
+}
