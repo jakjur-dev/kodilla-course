4c9c026572b7717382081aa0cc0bdf4f4853a0ce
Jakub <jakub.jurczak46@gmail.com>
2021-04-29 15:49:34 +0200

    17.2 Added TaskList and TaskListDao

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("Test list name", DESCRIPTION);
+        taskListDao.save(taskList);
+        String name = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(name);
+
+        //Then
+        assertEquals(1, readTaskList.size());
+
+        //CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+}
